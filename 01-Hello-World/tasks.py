6fd3c233fe8569ec9c624569dda2b983e26f4597
Sam Hart <shart@doximity.com>
2023-12-07 14:14:05 -0500

    basic test

--- 01-Hello-World/tasks.py
+++ 01-Hello-World/tasks.py
@@ -10,6 +10,7 @@ ext_list = ".list-txt"
 
 @mytask
 def build(c):
+    print(f"\n> Build...\n{'-' * 20}")
     inc_dir = os.getenv("DASMINC", ".")
     dasm_files = list(Path(c['work_dir']).glob('*.dasm'))
 
@@ -21,10 +22,17 @@ def build(c):
 
 @mytask
 def test(c):
-    raise NotImplementedError
+    print(f"\n> Test...\n{'-' * 20}")
+    gopher = os.getenv("GOPHER_BIN", "gopher2600")
+    bin_files = list(Path(c['work_dir']).glob(f'*{ext_bin}'))
+
+    for f in bin_files:
+        print(f"> Testing '{f}'")
+        c.run(f"{gopher} disasm {f.parts[-1]}")
 
 
 @mytask
 def clean(c):
+    print(f"\n> Clean...\n{'-' * 20}")
     c.run(f"rm -f *{ext_bin}")
     c.run(f"rm -f *{ext_list}")
